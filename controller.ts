2b0edad0ab904060570be02be7a2e7c0cb7c57d1
artёm rudoy <art.rudy@gmail.com>
2023-11-21 01:44:22 +0200

    error message during  login and registartion

--- controller.ts
+++ controller.ts
@@ -104,9 +104,17 @@ document.querySelector("#createUser")?.addEventListener("click", (e) => {
     const passwordRegex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    if (!passwordRegex.test(password)) {
+    const isFormValid =
+      login.trim() !== "" &&
+      name.trim() !== "" &&
+      surname.trim() !== "" &&
+      password.trim() !== "";
+
+    if (!isFormValid) {
+      registrationMessage.textContent = "Please fill in all fields.";
+    } else if (!passwordRegex.test(password)) {
       registrationMessage.textContent =
-        "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one special character.";
+        "Password must be at least 8  long and include at least one uppercase letter, one lowercase letter, and one special character.";
     } else {
       registrationMessage.textContent = "Registartion successful!";
       const user: User = {
@@ -115,13 +123,101 @@ document.querySelector("#createUser")?.addEventListener("click", (e) => {
         login: login,
         password: password,
       };
+
       createUser(user);
+
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
     }
   }
 
   document.body.appendChild(form);
 });
 
-document.querySelector("#login")?.addEventListener("click", () => {
-  console.log("log");
+document.querySelector("#login")?.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const loginMenu = document.querySelector("#userLogin") as HTMLElement;
+  if (loginMenu) loginMenu.style.display = "none";
+
+  const form = document.createElement("form");
+  form.id = "loginForm";
+
+  const loginLabel = document.createElement("label");
+  loginLabel.textContent = "Login:";
+  form.appendChild(loginLabel);
+
+  const loginInput = document.createElement("input");
+  loginInput.type = "text";
+  loginInput.id = "login";
+  loginInput.required = true;
+  loginInput.autocomplete = "off";
+  form.appendChild(loginInput);
+  form.appendChild(document.createElement("br"));
+
+  const passwordLabel = document.createElement("label");
+  passwordLabel.textContent = "Password:";
+  form.appendChild(passwordLabel);
+
+  const passwordInput = document.createElement("input");
+  passwordInput.type = "password";
+  passwordInput.id = "password";
+  passwordInput.required = true;
+  form.appendChild(passwordInput);
+  form.appendChild(document.createElement("br"));
+
+  const loginMessage = document.createElement("div");
+  loginMessage.classList.add("message");
+  form.appendChild(loginMessage);
+  form.appendChild(document.createElement("br"));
+
+  const submitButton = document.createElement("button");
+  submitButton.textContent = "Login";
+  submitButton.type = "button";
+  submitButton.addEventListener("click", validateLogin);
+  form.appendChild(submitButton);
+
+  function validateLogin() {
+    const login = loginInput.value;
+
+    const password = passwordInput.value;
+
+    const dataFromLocalStorage = localStorage.getItem(login);
+
+    if (dataFromLocalStorage) {
+      const passwordFromStorage = JSON.parse(dataFromLocalStorage).password;
+
+      if (password === passwordFromStorage) {
+        const currentUser = encodeURIComponent(
+          JSON.parse(dataFromLocalStorage).name
+        );
+        const currentLogin = encodeURIComponent(login);
+
+        window.location.href = `./activity.html?user=${currentUser}&login=${currentLogin}`;
+      } else {
+        loginMessage.textContent = "Password is not correct.";
+      }
+    } else {
+      loginMessage.textContent = "User is not found.";
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
+    }
+  }
+
+  document.body.appendChild(form);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const currentUser = urlParams.get("user");
+
+  const greetingElement = document.getElementById("greeting");
+
+  if (greetingElement) {
+    greetingElement.textContent = `Welcome, ${decodeURIComponent(
+      currentUser
+    )}.`;
+  }
 });
